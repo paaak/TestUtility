a734381716e9d032470000e1eb260f7866bc52c2
Paak <paaak@eteb.me>
2025-10-20 11:54:20 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/testutility/main.go
@@ -0,0 +1,23 @@
+// cmd/testutility/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "testutility/internal/testutility"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := testutility.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
